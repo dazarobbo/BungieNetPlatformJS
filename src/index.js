707296e8caaac8b5f13485864fe2ed8528ae6489
dazarobbo <_pm_me_on_bnet@bungie.net>
2017-01-06 15:16:40 +1100

    use es2017

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./BungieNet/Common.js";
 import { default as BungieNet } from "./BungieNet/BungieNet.js";
 import { default as Platform } from "./BungieNet/Platform.js";
 import { default as Plugin } from "./BungieNet/Plugin.js";
-export { BungieNet as default };
+export { BungieNet };
 
 BungieNet.Platform = Platform;
 BungieNet.Platform.Plugin = Plugin;
